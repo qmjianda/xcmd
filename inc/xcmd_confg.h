015899e7f56bfad9d13aa8a6d642750d368bd2c0
hqm <13720409820@163.com>
2021-09-17 14:40:40 +0800

    添加注释

--- inc/xcmd_confg.h
+++ inc/xcmd_confg.h
@@ -5,9 +5,9 @@
 extern   "C" {
 #endif
 
-#define XCMD_LINE_MAX_LENGTH    (32)
-#define XCMD_HISTORY_MAX_NUM    (0)
-#define XCMD_PARAM_MAX_NUM      (4)
+#define XCMD_LINE_MAX_LENGTH    (32) /* 命令行支持的最大字符数 */
+#define XCMD_HISTORY_MAX_NUM    (0)  /* 支持的历史记录个数，0为不支持 */
+#define XCMD_PARAM_MAX_NUM      (4)  /* 支持输入的参数个数 */
 
 #ifdef __cplusplus
         }
