5afe6fab7db5ae5dcd00bfb78493fda649f452ca
qmfr <13720409820@163.com>
2021-09-17 06:42:06 +0000

    update inc/xcmd_confg.h.

--- inc/xcmd_confg.h
+++ inc/xcmd_confg.h
@@ -6,7 +6,7 @@ extern   "C" {
 #endif
 
 #define XCMD_LINE_MAX_LENGTH    (32) /* 命令行支持的最大字符数 */
-#define XCMD_HISTORY_MAX_NUM    (0)  /* 支持的历史记录个数，0为不支持 */
+#define XCMD_HISTORY_MAX_NUM    (8)  /* 支持的历史记录个数，0为不支持 */
 #define XCMD_PARAM_MAX_NUM      (4)  /* 支持输入的参数个数 */
 
 #ifdef __cplusplus
