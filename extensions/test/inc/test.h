4d7ddecc3659cc000f7d70f875fd5e15e28394e0
hqm <13720409820@163.com>
2021-09-22 23:21:04 +0800

    添加扩展test

---
+++ extensions/test/inc/test.h
@@ -0,0 +1,30 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-22 22:33:24
+ * @LastEditTime: 2021-09-22 22:52:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /xcmd/extensions/test/test.h
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+#include "xcmd.h"
+
+#ifdef __cplusplus
+extern   "C" {
+#endif
+
+
+void test_cmd_init(void);
+void test_keys_init(void);
+
+#ifdef __cplusplus
+        }
+#endif
+
+
+#endif /* TEST_H */
+
+
