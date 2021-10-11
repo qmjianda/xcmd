c77220af8c5a11070e456a72d6a935ef9a451dc5
hqm <13720409820@163.com>
2021-10-11 21:54:38 +0800

    更新

--- extensions/ex_cmds/ex_cmds.c
+++ extensions/ex_cmds/ex_cmds.c
@@ -1,5 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-11 13:40:10
+ * @LastEditTime: 2021-10-11 13:41:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /xcmd/extensions/ex_cmds/ex_cmds.c
+ */
 #include "ex_cmds.h"
 #include "xcmd.h"
+
+#define HELP_RUN    ("Run cmd. Usage: run cmd")
 
 static int cmd_run(int argc, char* argv[])
 {
@@ -9,14 +19,14 @@ static int cmd_run(int argc, char* argv[])
     }
     else
     {
-        xcmd_print("usage: run \"cmd\"\n");
+        xcmd_print("%s\r\n", HELP_RUN);
     }
     return 0;
 }
 
 static xcmd_t cmds[] = 
 {
-    {"run", cmd_run, "run cmd", NULL},
+    {"run", cmd_run, HELP_RUN, NULL},
 };
 
 void ex_cmds_init(void)
