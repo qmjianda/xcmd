91fc4df5fd588ac1166f2d7dbf4afedf1f2a7208
hqm <13720409820@163.com>
2022-08-11 22:41:05 +0800

    优化： 为fatfs提供统一的设备注册接口，且在linux测试用例中测试通过

---
+++ example/linux/fatfs_disk_mmc.h
@@ -0,0 +1,8 @@
+#ifndef FATFS_DISK_MMC_H
+#define FATFS_DISK_MMC_H
+
+#include "ff.h"
+
+int mmc_disk_init(void);
+
+#endif
---
+++ example/linux/fatfs_disk_ram.h
@@ -0,0 +1,8 @@
+#ifndef FATFS_DISK_RAM_H
+#define FATFS_DISK_RAM_H
+
+#include "ff.h"
+
+int ram_disk_init(void);
+
+#endif
