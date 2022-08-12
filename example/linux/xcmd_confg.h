05192833eb7f19efea685b262c7d03facadffaff
hqm <13720409820@163.com>
2022-08-12 15:53:44 +0800

    优化： 优化了fatfs_port以及文件相关指令 1. fatfs_port支持设置卷标 2. 文件指令使用方式优化

--- example/linux/xcmd_confg.h
+++ example/linux/xcmd_confg.h
@@ -17,7 +17,7 @@ extern   "C" {
 
 #define XCMD_LINE_MAX_LENGTH            (128)           /* 命令行支持的最大字符数 */
 #define XCMD_HISTORY_MAX_NUM            (16)            /* 支持的历史记录个数, 这个参数对内存的影响很大，建议arduino设置为0，0为不支持 */
-#define XCMD_PARAM_MAX_NUM              (8)             /* 支持输入的参数个数 */
+#define XCMD_PARAM_MAX_NUM              (64)             /* 支持输入的参数个数 */
 // #define XCMD_DEFAULT_PROMPT           "->"            /* 提示符 */
 // #define XCMD_DEFAULT_PROMPT_CLOLR     TX_GREEN        /* 提示符颜色 */
 // #define ENABLE_XCMD_EXPORT                            /* 需要修改lds，启用后支持XCMD_EXPORT_CMD和XCMD_EXPORT_KEY方法导出命令 */
